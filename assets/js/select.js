178d3941147efe9a74728cba2ce74c1681385c4c
Smolik <vsmolik@ml000408271.active.local>
2014-04-21 18:11:31 -0700

    fixing search in dropdown

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,9 +1,13 @@
 $(function(){
 // sgd select
-	$('[data-dropdown-type="select"] li').on( "click", function(e) {
+	$('[data-dropdown-type="select"] li a').on( "click", function(e) {
 		var $target = $(e.currentTarget)
 		var selectedItem = $target.text();
 		$target.closest('.btn-group').find('[data-bind="label"]').text(selectedItem);
 		return true;
+	});
+
+	$('[data-dropdown-type="select"] li input').on( "click", function(e) {
+		return false;
 	});
 });
