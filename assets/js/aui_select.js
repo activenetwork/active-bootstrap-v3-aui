f7e972ee0f9522482027562bf1be25cc67dba7ad
Smolik <vsmolik@ml000408271.active.local>
2014-05-12 10:41:13 -0700

    fix for checkbox dropdown to support IE

--- assets/js/aui_select.js
+++ assets/js/aui_select.js
@@ -23,16 +23,9 @@ $(function(){
 		}		
 	});
 
-	$('.dropdown-toggle .checkbox').on( "click", function(e) {
-		var $target = $(e.target);
-		//if the checkbox is clicked prevent menu from opening
-		if ((e.offsetX > 1 && e.offsetX < 15 && !$target.is('.caret'))  
-		    || $target.is('input[type="checkbox"]')) { //stopping menu event causes checkbox event to be fired. Event needs to not be propagated after this
-			e.stopPropagation();
-		}	
-	 });
 
-	$('[data-toggle="dropdown"]').on('click', function() {
+
+	$('[data-toggle="dropdown"]').on('click', function(e) {
 		var $list = $($(this).closest('.btn-group').children('ul'));
 		var $searchInput = $($list.find('.typeahead'));
 		var $items = $list.find('a'); 
